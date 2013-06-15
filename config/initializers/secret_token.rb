18469e7cffdb9c06d175a1515b256d18f0f50b21
Matthew Forsyth <matt@mojotech.com>
2013-06-15 16:14:05 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tourneyitup::Application.config.secret_key_base = 'f67d64cebc5a5bca478f4c9db9f0e0e089b8ba562e60e65422e15d5c8d9429f0b5f23216a775793e3e9a3be876038f149b3a56c365b997d7157eaeb9e6fc6c53'
